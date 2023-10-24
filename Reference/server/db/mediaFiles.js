087285531c255171b62de1030a99cea50b1e4e70
veerappancts <112868339+veerappancts@users.noreply.github.com>
2023-10-24 14:57:19 +0000

    Delete Reference directory

--- Reference/server/db/mediaFiles.js
+++
@@ -1,7 +0,0 @@
-import { prisma } from ".";
-
-export const createMediaFile = (mediaFile) => {
-    return prisma.mediaFile.create({
-        data: mediaFile
-    })
-}
--- Reference/server/transformers/mediaFiles.js
+++
@@ -1,6 +0,0 @@
-export const mediaFileTransformer = (mediFile) => {
-    return {
-        id: mediFile.id,
-        url: mediFile.url
-    }
-}
